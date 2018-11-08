2bf880ca6994c12163c02b6bbc8ec131ff8d5840
Kunall Banerjee <yeskunall96@gmail.com>
2018-11-07 23:58:06 -0500

    Setup pro.ant.design

--- src/index.js
+++ src/index.js
@@ -1,78 +1,16 @@
 // @flow
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import styled, { createGlobalStyle } from 'styled-components';
 
 import * as serviceWorker from './serviceWorker';
+import { HomeContainer as App } from './containers/HomeContainer/index';
 
-const AbsolutelyCentered = styled.div`
-  left: 50%;
-  position: absolute;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const Container = styled.div`
-   {
-    /* Nothing here, but this is here for props */
-  }
-`;
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    background: #000;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  }
-`;
-
-const StyledButton = styled.button`
-  background: #fff;
-  border: 0;
-  border-radius: 1.5px;
-  color: #000;
-  cursor: pointer;
-  focus: 0;
-  font-size: 15px;
-  font-weight: bold;
-  height: 40px;
-  outline: none;
-  text-align: center;
-  text-transform: uppercase;
-  width: 200px;
-`;
-
-@observer
-class Now extends Component {
-  @observable now;
-
-  constructor(props) {
-    super(props);
-    this.start = this.start.bind(this);
-  }
-
-  start() {
-    this.now = Date.now();
-    console.log(`Started running @ ${this.now}`);
-  }
-
-  render() {
-    return (
-      <Container>
-        <GlobalStyle />
-        <AbsolutelyCentered>
-          <StyledButton onClick={this.start}>Click to start</StyledButton>
-        </AbsolutelyCentered>
-      </Container>
-    );
-  }
-}
+import 'antd/dist/antd.css';
 
 const root = document.getElementById('root');
 
 if (root != null) {
-  render(<Now />, root);
+  render(<App />, root);
 }
 
 // If you want your app to work offline and load faster, you can change
