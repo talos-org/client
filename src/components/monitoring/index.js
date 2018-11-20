312014008690f757577b9c4420ddb92e5f5b590c
abungay <amb240h@gmail.com>
2018-11-20 14:13:40 -0600

    Add user icon avatar to dashboard home header bar + add sample monitoring bizchart
    This avatar is clickable to show a dropdown allowing the user to log out.
    For now the log out feature simply redirects the user to the login page.
    
    Added placeholder monitoring content with sample bizchart.
    Added placeholder breadcrumbs to indicate current page user is on in dashboard.

---
+++ src/components/monitoring/index.js
@@ -0,0 +1,39 @@
+// @flow
+import * as React from 'react';
+import { Chart, Geom, Axis, Tooltip } from 'bizcharts';
+
+type Props = {};
+
+const sample_data = [
+  { stream: 'Cat Scan', size: 2400 },
+  { stream: "Driver's License", size: 655 },
+  { stream: 'Health Card', size: 980 },
+  { stream: 'X-Ray', size: 5320 },
+  { stream: 'Other', size: 3642 },
+];
+
+const cols = {
+  size: {
+    tickInterval: 1000,
+    alias: 'Size (KB)',
+  },
+  stream: {
+    alias: 'Stream Name',
+  },
+};
+
+export default class MonitoringComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Blockchain Storage Usage per Data Stream</h1>
+        <Chart width={800} height={600} data={sample_data} scale={cols}>
+          <Axis name="stream" title />
+          <Axis name="size" title />
+          <Tooltip />
+          <Geom type="interval" position="stream*size" />
+        </Chart>
+      </div>
+    );
+  }
+}
