3b84487d28fd6af5925bb40447dc8c7265e8489d
Kunall Banerjee <yeskunall96@gmail.com>
2018-12-03 17:32:11 -0500

    Use new app structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 // @flow
-import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import 'ant-design-pro/dist/ant-design-pro.css';
+
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import { render } from 'react-dom';
 
-import App from './routes/index';
+import App from 'screens/Root';
 import * as serviceWorker from './serviceWorker';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  *::after,*::before {
+    box-sizing: initial;
+  }
+  html {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 const root = document.getElementById('root');
-root.style.height = '100%';
 
 if (root != null) {
   render(
-    <Router>
-      <App />
-    </Router>,
+    <div>
+      <GlobalStyle />
+      <Router>
+        <App />
+      </Router>
+    </div>,
     root,
   );
 }
