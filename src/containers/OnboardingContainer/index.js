65df1e7efe1df25dc5506051c7c2ab882dad50c1
abungay <amb240h@gmail.com>
2018-12-04 01:37:41 -0600

    add header title with Talos logo and text to header of onboarding page

--- src/containers/OnboardingContainer/index.js
+++ src/containers/OnboardingContainer/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Col, Container, Row } from 'react-grid-system';
 import { Icon, Steps as _Steps } from 'antd';
 import { Redirect } from 'react-router-dom';
+import HeaderTitle from 'components/HeaderTitle/index';
 import styled from 'styled-components';
 
 import Form from 'components/Forms/Onboarding/index';
@@ -62,6 +63,7 @@ export default class OnboardingContainer extends React.Component<
               </Steps>
             </Col>
             <Col md={10}>
+              <HeaderTitle />
               <Form onUpdate={this.onUpdate} />
             </Col>
           </Row>
