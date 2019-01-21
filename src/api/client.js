4fd0606ce5f3af07eb2f724864a31306d949f33f
Kunall Banerjee <yeskunall96@gmail.com>
2019-01-21 12:41:13 -0500

    🌱 Make Flow happy

--- src/api/client.js
+++ src/api/client.js
@@ -5,43 +5,45 @@ const URL = process.env.REACT_APP_BASE_URL
   ? `http://${process.env.REACT_APP_BASE_URL}:5000/api`
   : 'http://localhost:5000/api';
 
-const getClient = (baseURL = URL) => {
+const getClient = (baseURL: string = URL) => {
   const options = {
     baseURL,
   };
 
-  const client = axios.create(options);
+  const client: AxiosInstance = axios.create(options);
 
   return client;
 };
 
 class APIClient {
-  constructor(baseURL = URL) {
+  client: AxiosInstance;
+
+  constructor(baseURL: string = URL) {
     this.client = getClient(baseURL);
   }
 
-  delete(url, conf = {}) {
+  delete(url: string, conf: Object = {}) {
     return this.client
       .delete(url, conf)
       .then(response => Promise.resolve(response))
       .catch(error => Promise.reject(error));
   }
 
-  get(url, conf = {}) {
+  get(url: string, conf: Object = {}) {
     return this.client
       .get(url, conf)
       .then(response => Promise.resolve(response))
       .catch(error => Promise.reject(error));
   }
 
-  post(url, data = {}, conf = {}) {
+  post(url: string, data: Object = {}, conf: Object = {}) {
     return this.client
       .post(url, data, conf)
       .then(response => Promise.resolve(response))
       .catch(error => Promise.reject(error));
   }
 
-  put(url, data = {}, conf = {}) {
+  put(url: string, data: Object = {}, conf: Object = {}) {
     return this.client
       .put(url, data, conf)
       .then(response => Promise.resolve(response))
