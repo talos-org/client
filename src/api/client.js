7ff7a8b363b2e5999550808addeadceaa1e9ed95
Kunall Banerjee <yeskunall96@gmail.com>
2019-01-21 12:44:29 -0500

    Integrate API client (#52)
    * feat(api): add new API client
    
    BREAKING CHANGE: Once this work is merged, how we make REST calls will change drastically. However,
    this change allows us to build a more robust REST client, which in the long run, will help us more
    than we know.
    
    * fix(api): use GCE VM’s external IPs
    
    * 🌱 Make Flow happy

---
+++ src/api/client.js
@@ -0,0 +1,54 @@
+// @flow
+import axios from 'axios';
+
+const URL = process.env.REACT_APP_BASE_URL
+  ? `http://${process.env.REACT_APP_BASE_URL}:5000/api`
+  : 'http://localhost:5000/api';
+
+const getClient = (baseURL: string = URL) => {
+  const options = {
+    baseURL,
+  };
+
+  const client: AxiosInstance = axios.create(options);
+
+  return client;
+};
+
+class APIClient {
+  client: AxiosInstance;
+
+  constructor(baseURL: string = URL) {
+    this.client = getClient(baseURL);
+  }
+
+  delete(url: string, conf: Object = {}) {
+    return this.client
+      .delete(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  get(url: string, conf: Object = {}) {
+    return this.client
+      .get(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  post(url: string, data: Object = {}, conf: Object = {}) {
+    return this.client
+      .post(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  put(url: string, data: Object = {}, conf: Object = {}) {
+    return this.client
+      .put(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+}
+
+export { APIClient };
