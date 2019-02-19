4d7efe6137de5df498376b44e54a405217e386d1
Kunall Banerjee <yeskunall96@gmail.com>
2019-02-19 05:55:23 -0500

    feat: use Rebass
    Finally moving towards a somewhat acceptable design “system.” Still a WIP, and needs a lot of work,
    but we’re getting there.
    
    BREAKING CHANGE: Using Rebass now.

---
+++ src/components/Welcome/index.styles.js
@@ -0,0 +1,13 @@
+// @flow
+import styled from 'styled-components';
+import { Box, Flex } from 'rebass';
+
+export const ButtonGroup = styled(Box)`
+  button {
+    margin-top: 7px;
+  }
+`;
+
+export const Container = styled(Flex)`
+  min-height: 100vh;
+`;
---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+export default {
+  breakpoints: {
+    sm: 0,
+    md: '48em',
+    lg: '80em',
+  },
+  colors: {
+    brightBlue: '#033FFF',
+    brightGreen: '#71EE17',
+    brightRed: '#F32626',
+    deepBlue: '#000F40',
+    white: '#FEFEFE',
+  },
+};
