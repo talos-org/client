cd6326d6689e91dd8906152714aee242bad152a8
Kunall Banerjee <yeskunall96@gmail.com>
2019-02-25 04:31:22 -0500

    test: more tests

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,8 +1,25 @@
-describe('/', () => {
+describe('accessing the app for the first time', () => {
+  beforeEach(() => {
+    cy.viewport('macbook-15');
+  });
+
   it('redirects to `/welcome`', () => {
-    // TODO: Inject `chainName` from local storage
-    // here and rewrite this test
     cy.visit('/');
     cy.url().should('include', 'welcome');
+  });
+});
+
+describe('accessing the app thereafter', () => {
+  beforeEach(() => {
+    cy.viewport('macbook-15');
+    window.localStorage.setItem('chainName', '__DEBUG__');
+  });
+
+  it('takes the user to the dashboard', () => {
+    cy.visit('/');
+  });
+
+  it('can see the word “__DEBUG__” somewhere on the screen', () => {
+    cy.contains('__DEBUG__');
   });
 });
