62e1c5806215f5145fc9f5554a86ba20f735498b
Kunall Banerjee <yeskunall96@gmail.com>
2019-03-13 18:15:16 -0400

    refactor(monitoring): various improvements to the monitoring tab

--- src/api/monitoring.js
+++ src/api/monitoring.js
@@ -3,10 +3,10 @@ import { APIClient } from 'api/client';
 
 let client = new APIClient();
 
-export function getPeerInfo(name: string) {
-  return client.get(`/get_peer_info?blockchainName=${name}`);
+export async function getPeerInfo(name: string) {
+  return client.get(`/network/get_peer_info?blockchainName=${name}`);
 }
 
-export function getInactiveNodes(data: { blockchainName: string }) {
-  return client.post('/get_inactive_nodes', data);
+export function getWalletAddress(name: string) {
+  return client.get(`/network/get_wallet_address?blockchainName=${name}`);
 }
